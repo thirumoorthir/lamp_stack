1b43a48503adc4281ebc0d22798481410f06ddfb
Thiru Moorthy <moorthy978@gmail.com>
2019-12-11 08:44:48 +0530

    adding new recipe for jenkins install

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+file '/var/www/html/index.html' do
+    content "Welcome to My project\n"
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+
+package 'apache2' do
+    action :install
+end
+
+package 'elinks' do
+    action :install
+end
+
+
